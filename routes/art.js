9083ddc106e95c26ebc420f74c00ca67cda08f2a
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-13 19:39:33 -0800

    Add getDepartmentId function
    Add getDepartmentId function that returns the department id for
    a given department name.

--- routes/art.js
+++ routes/art.js
@@ -2,7 +2,17 @@ const express = require("express");
 const router = express.Router();
 const departmentName = "Egyptian Art";
 
-router.get("/", (req, res) => {
+async function getDepartmentId(departmentName) {
+    const response = await fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments");
+    const data = await response.json();
+    const id = data.departments.find(department => {
+        return department.displayName == departmentName;
+    }).departmentId;
+    return id;
+}
+
+router.get("/", async (req, res) => {
+    const departmentId = await getDepartmentId(departmentName);
     res.render("index", {departmentName: departmentName});
 });
 

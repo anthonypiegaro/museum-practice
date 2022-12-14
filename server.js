fff6dcbfbfda02f4ebb8c6fa8be5c1f7aff3e817
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-13 21:45:26 -0800

    Add script.js and style.css and link static files to server.

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const artRouter = require("./routes/art");
 
 app.use("/", artRouter);
 
+// Static files
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css" ));
+app.use("/js", express.static(__dirname + "public/js" ));
+
 app.listen(PORT, (error) => {
     if (!error) {
         console.log("Server running successfully. Listening on port " + PORT);

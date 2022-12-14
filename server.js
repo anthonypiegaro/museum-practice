80f64f562e12f469c00c88a003c5a34007edc356
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-13 18:56:01 -0800

    Add route for art page
    Add art.js to routes to create a route. Add index.ejs template
    to views. Add route to server.

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
+const artRouter = require("./routes/art");
+
+app.use("/", artRouter);
+
 app.listen(PORT, (error) => {
     if (!error) {
         console.log("Server running successfully. Listening on port " + PORT);

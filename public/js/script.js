8a922e8704d47e51151a8343ed797b30d95f1d8f
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-13 21:51:00 -0800

    Add getImages function to script.js
    getImages calls the /getImage route and gets the department
    images.

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+async function getImages() {
+    const response = await fetch("http://localhost:3000/getImages");
+    const data = await response.json();
+    console.log(data.images);
+    return data.images
+}
+
+getImages();

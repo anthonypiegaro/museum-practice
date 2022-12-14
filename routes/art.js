e604e36833860b7c1091e9e0bae3227c18635617
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-13 19:30:00 -0800

    Add department name
    Add department name to route file and pass to route. Update index
    file and add header with department name to template.

--- routes/art.js
+++ routes/art.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const departmentName = "Egyptian Art";
 
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {departmentName: departmentName});
 });
 
 module.exports = router;

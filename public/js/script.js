8e9a0be5162f07808e44e54fe37b54262c7824b5
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-14 09:46:47 -0800

    Add intro animation to art page
    Add intro content to template. Add styling to css. Add function
    to script.js that adds and removes classes to run the animation
    when the screen loads.

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,12 @@
 const contentWrapper = document.querySelector(".content-wrapper");
+const intro = document.querySelector(".intro");
+const logo = document.querySelector(".logo-header");
+const logoSpan = document.querySelectorAll(".logo");
 
 async function getImages() {
     const response = await fetch("http://localhost:3000/getImages");
     const data = await response.json();
-    return data.images
+    return data.images;
 }
 
 async function createImages() {
@@ -21,3 +24,23 @@ async function createImages() {
 }
 
 createImages();
+
+// Creates intro
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add("active");
+            }, (idx + 1) * 600)
+        });
+        setTimeout(() => {
+            logoSpan.forEach((span, idx) => {
+                span.classList.remove("active");
+                span.classList.add("fade");
+            }, (idx = 1) * 50)
+        }, 3500);
+        setTimeout(() => {
+            intro.style.top = "-100vh";
+        }, 3800);
+    })
+});

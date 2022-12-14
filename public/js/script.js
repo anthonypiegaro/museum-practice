41fcdbc4cfe76247b535bc339f883b24507e7560
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-13 22:02:12 -0800

    Add createImages function to script.js
    createImages adds the images to the DOM

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,23 @@
+const contentWrapper = document.querySelector(".content-wrapper");
+
 async function getImages() {
     const response = await fetch("http://localhost:3000/getImages");
     const data = await response.json();
-    console.log(data.images);
     return data.images
 }
 
-getImages();
+async function createImages() {
+    const images = await getImages();
+    images.forEach(image => {
+        const placard = document.createElement("div");
+        placard.classList.add("placard-wrapper");
+        const imageContent = document.createElement("img");
+        imageContent.setAttribute("src", image);
+        imageContent.setAttribute("alt", "Egyptian Artwork");
+        imageContent.classList.add("artwork");
+        placard.appendChild(imageContent);
+        contentWrapper.appendChild(placard);
+    });
+}
+
+createImages();

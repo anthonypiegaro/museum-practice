08fa276e04d8c4a354ac2c221a4244da3c54d233
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2022-12-13 21:16:20 -0800

    Add route that sends random images in json

--- routes/art.js
+++ routes/art.js
@@ -4,9 +4,13 @@ const returnImages = require("../returnImages");
 const departmentName = "Egyptian Art";
 
 router.get("/", async (req, res) => {
-    const images = await returnImages(departmentName);
-    console.log(images);
     res.render("index", {departmentName: departmentName});
 });
+
+router.get("/getImages", async (req, res) => {
+    const images = await returnImages(departmentName);
+    const data = {images: Array.from(images)}
+    res.json(data)
+})
 
 module.exports = router;
